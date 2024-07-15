a860c62c3d4db3a90ab6b7e08d56f2d41a385390
SumorockYeah2 <sumorockyeah2@gmail.com>
2024-07-15 10:54:49 +0700

    check delete event dialog

---
+++ src/app/pages/calendar/delete-event-dialog/delete-event-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-event-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './delete-event-dialog.component.html',
+  styleUrl: './delete-event-dialog.component.scss'
+})
+export class DeleteEventDialogComponent {
+
+}
